716947345b9dfd9036a7b26bc2d14c0dbeb5c8a3
Sebastian Aguiar <104685013+SebaAguiar@users.noreply.github.com>
2024-04-24 19:26:01 -0300

    some state corrections - typos corrections

--- src/components/BurgerMenu.tsx
+++ src/components/BurgerMenu.tsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
+import { useStore } from '@nanostores/react';
 import { LuX, LuAlignJustify } from 'react-icons/lu';
 import { isOpen } from '../nanostores/store';
 
-const BurguerMenu = () => {
-	const [local, setLocal] = useState(isOpen.value);
+const BurgerMenu = () => {
+	const open = useStore(isOpen);
 
 	const toggleSide = () => {
 		isOpen.set(!isOpen.value);
-		setLocal(!local);
 	};
 	// console.log(isOpen.value);
 	return (
@@ -16,10 +16,10 @@ const BurguerMenu = () => {
 				onClick={toggleSide}
 				className='text-dark-white fixed right-1 top-1 z-50 flex size-10 items-center justify-center text-lg transition-all duration-500 ease-in-out lg:hidden'
 			>
-				{local ? <LuX /> : <LuAlignJustify />}
+				{open ? <LuX /> : <LuAlignJustify />}
 			</button>
 		</>
 	);
 };
 
-export default BurguerMenu;
+export default BurgerMenu;
